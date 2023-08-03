ef8d058724f9627c6357bb2e5b47b954c586101c
xabi1000 <babelwado@gmail.com>
2023-08-03 11:47:59 +0100

    change netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			edge: false,
-			split: false
+			// will create a Netlify Edge Function using Deno-based
+			// rather than using standard Node-based functions
+			edge: true
 		})
 	}
 };
-
-export default config;
